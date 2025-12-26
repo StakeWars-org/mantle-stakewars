4b9ac3a2db412e301a748f1d3c750f3dd391d840
doye <edyogoba23@gmail.com>
2025-12-26 18:36:04 +0100

    Update Next.js configuration to include additional Firebase environment variables and add @react-native-async-storage/async-storage dependency in package.json.

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,13 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID
-  },
-  serverExternalPackages: ['thread-stream', 'pino'],
-  turbopack: {
-    resolveAlias: {
-      'tap': path.join(__dirname, 'empty-module.js'),
-      'why-is-node-running': path.join(__dirname, 'empty-module.js'),
-      '@react-native-async-storage/async-storage': path.join(__dirname, 'empty-module.js'),
-    },
+    NEXT_PUBLIC_PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID,
+    FIREBASE_API_ID: process.env.FIREBASE_API_ID,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY
   },
 };
 
